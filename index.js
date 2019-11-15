7fd8a7ee6a39a5d9c9044547365c08be47a9c302
Fernando Franco <atr.franco@gmail.com>
2019-11-15 19:03:26 -0300

    Fixed fullpage in mobile

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ import './src/style.css';
 const FULLPAGE_STYLE = `
 html, body {
   margin: 0;
-  height: 100%;
+  height: 100vh;
 }
 .u-container {
-  height: 100%;
+  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
 }
 `;
 
@@ -22,6 +23,14 @@ export default {
       const styles = document.createElement('style');
       styles.innerHTML = FULLPAGE_STYLE;
       document.getElementsByTagName('head')[0].appendChild(styles);
+
+      function setVhVariable() {
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+      }
+
+      setVhVariable();
+      window.addEventListener('resize', setVhVariable);
     }
   },
 };
